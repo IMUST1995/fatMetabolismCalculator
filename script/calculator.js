8c375e4d4793ba50c2ae4510f17b0657163ca0e7
IMUST1995 <zeirbraidi@gmail.com>
2022-11-16 13:57:50 -0700

    kg/week

--- script/calculator.js
+++ script/calculator.js
@@ -18,5 +18,9 @@ function maximunDailyCaloricDeficit(lbsOfFat){
     const DailyCaloricDeficit = lbsOfFat * 31
     console.log(DailyCaloricDeficit)
     const DailyCaloricDeficitHolder = document.getElementById('DailyCaloricDeficitHolder')
-    DailyCaloricDeficitHolder.innerHTML = `${DailyCaloricDeficit.toFixed(2)} Maximum daily calorie deficit from fat`
+    DailyCaloricDeficitHolder.innerHTML = `${DailyCaloricDeficit.toFixed(0)} Maximum daily calorie deficit from fat`
+    const maxAmountOfFatWeekly = (DailyCaloricDeficit * 7) / 3500
+    DailyCaloricDeficitHolder.innerHTML += `<h4>${maxAmountOfFatWeekly.toFixed(2)} lbs of fat / week</h4>`
+    const maxAmountOfFatInKg = maxAmountOfFatWeekly * 0.453592
+    DailyCaloricDeficitHolder.innerHTML += `<h4>${maxAmountOfFatInKg.toFixed(2)} kg of fat / week</h4>`
 }
